f689426b2aaedff52b724bf4e8d0f9e68a2b204e
ryanf10 <ryanfernaldy10@gmail.com>
2023-11-11 08:31:08 +0700

    feat: update next sitemap

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: 'Template',
   description: 'A starter admin template',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'http://157.245.58.12:3005',
 };
