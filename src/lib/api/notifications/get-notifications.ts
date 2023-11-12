bc18b2790ee8498c93f21b2df504eaef61da5134
ryanf10 <ryanfernaldy10@gmail.com>
2023-11-12 12:53:18 +0700

    feat: add enabled query notification

--- src/lib/api/notifications/get-notifications.ts
+++ src/lib/api/notifications/get-notifications.ts
@@ -2,15 +2,19 @@ import { QueryFunction, useQuery } from '@tanstack/react-query';
 
 import axios from '@/lib/axios';
 
+import useNotificationStore from '@/store/useNotificationStore';
+
 import { ApiResponse } from '@/types/api';
 import { Notification } from '@/types/notification';
 
 export const getNotificationsKey = 'get-notifications-key';
 export const useGetNotifications = () => {
+  const notifications = useNotificationStore.useNotifications();
   return useQuery({
     queryKey: [getNotificationsKey],
     queryFn: getNotifications,
     retry: 3,
+    enabled: !notifications,
   });
 };
 export const getNotifications: QueryFunction<
