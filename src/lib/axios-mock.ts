89875becc01df5d7adea0a94c859e81d0598f5dc
ryanf10 <ryanfernaldy10@gmail.com>
2023-12-03 15:04:20 +0700

    fix: mock base url

--- src/lib/axios-mock.ts
+++ src/lib/axios-mock.ts
@@ -5,9 +5,9 @@ import { UninterceptedApiError } from '@/types/api';
 
 /** Add NEXT_PUBLIC_MOCK_DEPLOYMENT_URL to your production deployment on vercel! */
 const baseURL = process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL
-  ? `https://${process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL}/api/mock`
+  ? `${process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL}/api/mock`
   : process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/mock`
+  ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/mock`
   : 'http://localhost:3001/api/mock';
 
 export const apiMock = axios.create({
