da57c2a02767626ecc4d532edc35f08b2fe14a61
ryanf10 <ryanfernaldy10@gmail.com>
2023-12-03 14:30:47 +0700

    chore: upgrade to immer 10.0.3

--- src/store/useDialogStore.tsx
+++ src/store/useDialogStore.tsx
@@ -1,5 +1,5 @@
 import { createSelectorHooks } from 'auto-zustand-selectors-hook';
-import produce from 'immer';
+import { produce } from 'immer';
 import { create } from 'zustand';
 
 import { DialogOptions } from '@/components/dialog/BaseDialog';
