8821c0e445c653e25ea36409d1e0641861187c08
ryanf10 <ryanfernaldy10@gmail.com>
2024-02-26 09:14:46 +0700

    chore: add react-query-devtools

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 import React from 'react';
 
@@ -21,6 +22,7 @@ function Providers({ children }: React.PropsWithChildren) {
         <DismissibleToast />
         <Dialog />
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
